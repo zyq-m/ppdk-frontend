7efab70d8618a67989895bcbb3f240cc5e03fea3
haziq <haziq.musa02@gmail.com>
2024-12-27 18:12:18 +0800

    update chart

--- src/pages/admin-ppdk/index.tsx
+++ src/pages/admin-ppdk/index.tsx
@@ -17,6 +17,14 @@ import {
 } from "@/components/ui/card";
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 import { TrendingUp } from "lucide-react";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 const chartConfig = {
 	lelaki: {
@@ -63,12 +71,35 @@ const chartData = [
 ];
 
 export default function DashboardAdminPPK() {
+	const [timeRange, setTimeRange] = useState("90d");
+
 	return (
 		<Layout>
 			<Card>
-				<CardHeader>
-					<CardTitle>Graf Bar - Pelatih</CardTitle>
-					<CardDescription>Januari - Jun 2024</CardDescription>
+				<CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+					<div className="grid flex-1 gap-1 text-center sm:text-left">
+						<CardTitle>Graf Bar - Pelatih</CardTitle>
+						<CardDescription>Januari - Jun 2024</CardDescription>
+					</div>
+					<Select value={timeRange} onValueChange={setTimeRange}>
+						<SelectTrigger
+							className="w-[160px] rounded-lg sm:ml-auto"
+							aria-label="Select a value"
+						>
+							<SelectValue placeholder="Last 3 months" />
+						</SelectTrigger>
+						<SelectContent className="rounded-xl">
+							<SelectItem value="90d" className="rounded-lg">
+								Last 3 months
+							</SelectItem>
+							<SelectItem value="30d" className="rounded-lg">
+								Last 30 days
+							</SelectItem>
+							<SelectItem value="7d" className="rounded-lg">
+								Last 7 days
+							</SelectItem>
+						</SelectContent>
+					</Select>
 				</CardHeader>
 				<CardContent>
 					<ChartContainer config={chartConfig}>
--- src/pages/admin-ppdk/penilaian/index.tsx
+++ src/pages/admin-ppdk/penilaian/index.tsx
@@ -15,7 +15,15 @@ import {
 	ChartLegend,
 	ChartLegendContent,
 } from "@/components/ui/chart";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 import { TrendingUp } from "lucide-react";
+import { useState } from "react";
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 
 const chartConfig = {
@@ -63,51 +71,94 @@ const chartData = [
 ];
 
 export default function DashboardPenilaian() {
+	const [score, setScore] = useState("39");
+	const [timeRange, setTimeRange] = useState("90d");
+
 	return (
 		<Layout>
 			<Card>
-				<CardHeader>
-					<CardTitle>Graf Bar - Penilaian</CardTitle>
-					<CardDescription>Januari - Jun 2024</CardDescription>
-					<CardContent>
-						<ChartContainer config={chartConfig}>
-							<BarChart accessibilityLayer data={chartData}>
-								<CartesianGrid vertical={false} />
-								<XAxis
-									dataKey="month"
-									tickLine={false}
-									tickMargin={10}
-									axisLine={false}
-									tickFormatter={(value) => value.slice(0, 3)}
-								/>
-								<ChartTooltip
-									content={<ChartTooltipContent />}
-								/>
-								<ChartLegend content={<ChartLegendContent />} />
-								<Bar
-									dataKey="k1"
-									fill="var(--color-k1)"
-									radius={4}
-								/>
-								<Bar
-									dataKey="k2"
-									fill="var(--color-k2)"
-									radius={4}
-								/>
-							</BarChart>
-						</ChartContainer>
-					</CardContent>
-					<CardFooter className="flex-col items-start gap-2 text-sm">
-						<div className="flex gap-2 font-medium leading-none">
-							Trending up by 5.2% this month{" "}
-							<TrendingUp className="h-4 w-4" />
-						</div>
-						<div className="leading-none text-muted-foreground">
-							Menunjukkan jumlah pelatih berdaftar sepanjang 6
-							bulan lepas
-						</div>
-					</CardFooter>
+				<CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+					<div className="grid flex-1 gap-1 text-center sm:text-left">
+						<CardTitle>Graf Bar- Penilaian</CardTitle>
+						<CardDescription>Januari - Jun 2024</CardDescription>
+					</div>
+					<div className="flex gap-2">
+						<Select value={timeRange} onValueChange={setTimeRange}>
+							<SelectTrigger
+								className="w-[160px] rounded-lg sm:ml-auto"
+								aria-label="Select a value"
+							>
+								<SelectValue placeholder="Last 3 months" />
+							</SelectTrigger>
+							<SelectContent className="rounded-xl">
+								<SelectItem value="90d" className="rounded-lg">
+									Last 3 months
+								</SelectItem>
+								<SelectItem value="30d" className="rounded-lg">
+									Last 30 days
+								</SelectItem>
+								<SelectItem value="7d" className="rounded-lg">
+									Last 7 days
+								</SelectItem>
+							</SelectContent>
+						</Select>
+						<Select value={score} onValueChange={setScore}>
+							<SelectTrigger
+								className="w-[160px] rounded-lg sm:ml-auto"
+								aria-label="Select a value"
+							>
+								<SelectValue placeholder="Skor" />
+							</SelectTrigger>
+							<SelectContent className="rounded-xl">
+								<SelectItem value="39" className="rounded-lg">
+									Skor 39% ke bawah
+								</SelectItem>
+								<SelectItem value="40" className="rounded-lg">
+									Skor antara 40% - 60%
+								</SelectItem>
+								<SelectItem value="61" className="rounded-lg">
+									Skor antara 61% - 100%
+								</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
 				</CardHeader>
+				<CardContent>
+					<ChartContainer config={chartConfig}>
+						<BarChart accessibilityLayer data={chartData}>
+							<CartesianGrid vertical={false} />
+							<XAxis
+								dataKey="month"
+								tickLine={false}
+								tickMargin={10}
+								axisLine={false}
+								tickFormatter={(value) => value.slice(0, 3)}
+							/>
+							<ChartTooltip content={<ChartTooltipContent />} />
+							<ChartLegend content={<ChartLegendContent />} />
+							<Bar
+								dataKey="k1"
+								fill="var(--color-k1)"
+								radius={4}
+							/>
+							<Bar
+								dataKey="k2"
+								fill="var(--color-k2)"
+								radius={4}
+							/>
+						</BarChart>
+					</ChartContainer>
+				</CardContent>
+				<CardFooter className="flex-col items-start gap-2 text-sm">
+					<div className="flex gap-2 font-medium leading-none">
+						Trending up by 5.2% this month{" "}
+						<TrendingUp className="h-4 w-4" />
+					</div>
+					<div className="leading-none text-muted-foreground">
+						Menunjukkan jumlah pelatih berdaftar sepanjang 6 bulan
+						lepas
+					</div>
+				</CardFooter>
 			</Card>
 		</Layout>
 	);
