659edf3e0abf1380a1cc225e3c3c1e2b7aff7902
haziq <haziq.musa02@gmail.com>
2025-01-06 12:55:57 +0800

    update login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -26,6 +26,23 @@ const formSchema = z.object({
 });
 
 export default function Login() {
+	return (
+		<div className="grid min-h-svh lg:grid-cols-2">
+			<div className="w-full max-w-lg  mx-auto">
+				<LoginForm />
+			</div>
+			<div className="relative hidden lg:block">
+				<img
+					src="https://images.pexels.com/photos/764681/pexels-photo-764681.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+					alt="Image"
+					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+				/>
+			</div>
+		</div>
+	);
+}
+
+function LoginForm() {
 	const navigate = useNavigate();
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -59,45 +76,48 @@ export default function Login() {
 			console.log(error);
 		}
 	}
-
 	return (
-		<div className="max-w-md mx-auto">
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className="flex flex-col gap-2 justify-center min-h-screen px-4"
-				>
-					<FormField
-						control={form.control}
-						name="email"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input type="email" {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="password"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Kata laluan</FormLabel>
-								<FormControl>
-									<Input type="password" {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button className="mt-4" type="submit">
-						Login
-					</Button>
-				</form>
-			</Form>
-		</div>
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="flex flex-col gap-2 justify-center min-h-screen px-4"
+			>
+				<h1 className="text-2xl font-bold text-center">
+					Selamat datang ke PPDK InfoSys
+				</h1>
+				<p className="text-balance text-sm text-muted-foreground text-center">
+					Masukkan email untuk log masuk
+				</p>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input type="email" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Kata laluan</FormLabel>
+							<FormControl>
+								<Input type="password" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button className="mt-4" type="submit">
+					Login
+				</Button>
+			</form>
+		</Form>
 	);
 }
